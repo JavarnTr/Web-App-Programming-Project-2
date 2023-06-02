31a2615b4e5181caeeb17f3625d684126f79ad85
JavarnTr <83565578+JavarnTr@users.noreply.github.com>
2023-06-02 15:46:15 +1200

    home

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
-include 'includes/header.html';
+<?php include 'includes/header.html';?>
 
-?>
+<head>
+    <link rel="stylesheet" type="text/css" href="includes/style.css">
+</head>
+
+<div class="content">
+    <h1 class="title">Welcome to PureTech</h1>
+    <div class="store_button">
+        <button><a href="store.php">Shop Now</a></button>
+    </div>
+</div>
+
+<?php include 'includes/footer.html';?>
