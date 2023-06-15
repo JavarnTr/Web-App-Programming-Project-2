1ebcb6c663fca4e015b3bf8e9dc3aa0ee6fe28f3
JavarnTr <83565578+JavarnTr@users.noreply.github.com>
2023-06-15 16:32:35 +1200

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -39,6 +39,55 @@ create table order_details (
     FOREIGN KEY(orderID) REFERENCES orders(orderID)
 );
 
+create table posts (
+    postID INT AUTO_INCREMENT PRIMARY KEY,
+    threadID INT,
+    userID INT,
+    message VARCHAR(200),
+    datePosted DATE,
+    FOREIGN KEY(userID) REFERENCES users(userID),
+    FOREIGN KEY(threadID) REFERENCES threads(threadID)
+);
+
+create table threads (
+    threadID INT AUTO_INCREMENT PRIMARY KEY,
+    userID INT,
+    languageID INT,
+    subject VARCHAR(50),
+    FOREIGN KEY(userID) REFERENCES users(userID),
+    FOREIGN KEY(languageID) REFERENCES languages(languageID)
+);
+
+create table languages (
+    languageID INT AUTO_INCREMENT PRIMARY KEY,
+    language VARCHAR(50),
+    lang_eng VARCHAR(50)
+);
+
+create table words (
+    wordID INT AUTO_INCREMENT PRIMARY KEY,
+    languageID INT,
+    title VARCHAR(50),
+    intro VARCHAR(60),
+    home VARCHAR(60),
+    forum_home VARCHAR(40),
+    language VARCHAR(50),
+    register VARCHAR(50),
+    login VARCHAR(50),
+    logout VARCHAR(50),
+    new_thread VARCHAR(50),
+    subject VARCHAR(50),
+    body VARCHAR(50),
+    submit VARCHAR(50),
+    posted_on VARCHAR(50),
+    posted_by VARCHAR(50),
+    replies VARCHAR(50),
+    latest_reply VARCHAR(50),
+    post_a_reply VARCHAR(50),
+    FOREIGN KEY(languageID) REFERENCES languages(languageID)
+);
+
+
 insert into products(productID, productName, price, description, inventory, image)
 values (1, 'iPhone 8', '129.99', 'Reliable phone', '38', 'images/iphone8');
 
