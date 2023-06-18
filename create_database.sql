e62b2153be1977aa747bace5c588d6671e54c7d9
JavarnTr <83565578+JavarnTr@users.noreply.github.com>
2023-06-18 17:37:48 +1200

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -1,19 +1,10 @@
 drop table if exists order_details;
 drop table if exists products;
 drop table if exists orders;
-
-
 drop table if exists posts;
 drop table if exists threads;
-drop table if exists words;
 drop table if exists users;
-drop table if exists languages;
 
-create table languages (
-    languageID INT AUTO_INCREMENT PRIMARY KEY,
-    language VARCHAR(50),
-    lang_eng VARCHAR(50)
-);
 
 create table users (
     userID INT AUTO_INCREMENT PRIMARY KEY,
@@ -21,10 +12,7 @@ create table users (
     lastName VARCHAR(50),
     email VARCHAR(50),
     password VARCHAR(50),
-    languageID INT,
-    time_zone VARCHAR(50),
-    registration_date DATE,
-    FOREIGN KEY(languageID) REFERENCES languages(languageID)
+    registration_date DATE,  
 );
 
 create table products (
@@ -57,10 +45,8 @@ create table order_details (
 create table threads (
     threadID INT AUTO_INCREMENT PRIMARY KEY,
     userID INT,
-    languageID INT,
     subject VARCHAR(50),
     FOREIGN KEY(userID) REFERENCES users(userID),
-    FOREIGN KEY(languageID) REFERENCES languages(languageID)
 );
 
 create table posts (
@@ -73,48 +59,15 @@ create table posts (
     FOREIGN KEY(threadID) REFERENCES threads(threadID)
 );
 
-create table words (
-    wordID INT AUTO_INCREMENT PRIMARY KEY,
-    languageID INT,
-    title VARCHAR(50),
-    intro VARCHAR(60),
-    home VARCHAR(60),
-    forum_home VARCHAR(40),
-    language VARCHAR(50),
-    register VARCHAR(50),
-    login VARCHAR(50),
-    logout VARCHAR(50),
-    new_thread VARCHAR(50),
-    subject VARCHAR(50),
-    body VARCHAR(50),
-    submit VARCHAR(50),
-    posted_on VARCHAR(50),
-    posted_by VARCHAR(50),
-    replies VARCHAR(50),
-    latest_reply VARCHAR(50),
-    post_a_reply VARCHAR(50),
-    FOREIGN KEY(languageID) REFERENCES languages(languageID)
-);
 
 insert into products(productID, productName, price, description, inventory, image)
 values (1, 'iPhone 8', '129.99', 'Reliable phone', '38', 'images/iphone8');
 
-insert into languages(language, lang_eng)
-values ('English', 'English'),
-    ('Português', 'Portuguese'),
-    ('Français', 'French'),
-    ('Norsk', 'Norwegian'),
-    ('Romanian', 'Romanian'),
-    ('Deutsch', 'German'),
-    ('Srpski', 'Serbian'),
-    ('Nederlands', 'Dutch');
+insert into users(userID, firstName, lastName, email, password, registration_date)
+values (1, 'John', 'Doe', 'johndoe@gmail.com', SHA1('password'), '2018-01-01'),
+    (2, 'Jane', 'Doe', 'janedoe@gmail.com', SHA1('password'), '2018-01-01'),
+    (3, 'Jane', 'Doe', 'janedoe@gmail.com', SHA1('password'), '2018-01-01'),
+    (4, 'Jane', 'Doe', 'janedoe@gmail.com', SHA1('password'), '2018-01-01'),
+    (5, 'Jane', 'Doe', 'janedoe@gmail.com', SHA1('password'), '2018-01-01');
 
-insert into users(userID, firstName, lastName, email, password, registration_date, languageID, time_zone)
-values (1, 'John', 'Doe', 'johndoe@gmail.com', SHA1('password'), '2018-01-01', 1, 'America/New_York'),
-    (2, 'Jane', 'Doe', 'janedoe@gmail.com', SHA1('password'), '2018-01-01', 7, 'Europe/Berlin'),
-    (3, 'Jane', 'Doe', 'janedoe@gmail.com', SHA1('password'), '2018-01-01', 4, 'Europe/Oslo'),
-    (4, 'Jane', 'Doe', 'janedoe@gmail.com', SHA1('password'), '2018-01-01', 2, 'America/Sao_Paulo'),
-    (5, 'Jane', 'Doe', 'janedoe@gmail.com', SHA1('password'), '2018-01-01', 1, 'Pacific/Auckland');
 
-INSERT INTO words VALUES
-(NULL, 1, 'Forum','<p>Welcome to the Forum</p>', 'Home', 'Forum Home', 'Language', 'Register', 'Login', 'Logout', 'New Thread', 'Subject', 'Body', 'Submit', 'Posted on', 'Posted by', 'Replies', 'Latest Reply', 'Post a Reply');
